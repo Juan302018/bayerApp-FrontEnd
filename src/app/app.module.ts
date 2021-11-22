bb76c1277270e0b73c866c4521cc36517ba58f50
Juan Fernandez <juan.fernandezv@sonda.com>
2021-11-22 05:05:40 -0300

    configuracion inicial bayerApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { ToastrModule } from 'ngx-toastr';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
+import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbDatepickerModule, NgbModalModule, NgbModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,7 +22,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    CommonModule,
+    FlexLayoutModule,
+    NgOptionHighlightModule,
+    NgMultiSelectDropDownModule,
+    NgxPaginationModule,
+    NgxDatatableModule,
+    FontAwesomeModule,
+    ToastrModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbDatepickerModule,
+    NgbModalModule,
+    NgSelectModule,
+    NgbModule,
+    NgbTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
