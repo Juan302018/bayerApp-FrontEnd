04add5773e07bce7b812097ddcd53ca25b5943f3
Juan302018 <jfernandez816@hotmail.com>
2022-01-04 16:55:53 -0300

    Avance de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public hide: boolean = false;
+  public showEye: Boolean = false;
+  public hideConf: boolean = false;
+  public showEyeConf: Boolean = false;
+
+  username: string;
+  password: string;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  iniciarSesion() {
+    console.log('Inicio sesión!');
+    setTimeout(() => {
+      this.router.navigate(['catalogo']);
+    }, 2000);
+  }
+
+  mostrarPassword() {
+    this.hide = !this.hide;
+    this.showEye = !this.showEye;
   }
 
 }
