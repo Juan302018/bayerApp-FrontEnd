f56ab73749e526727007af88331ca9fe5a3887bb
Camilo Oviedo <camilo.oviedof@gmail.com>
2022-01-05 20:57:23 -0300

    Validaciones en formulario de login con respectivas alertas

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators, FormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -13,19 +14,22 @@ export class LoginComponent implements OnInit {
   public hideConf: boolean = false;
   public showEyeConf: Boolean = false;
 
-  username: string;
-  password: string;
+  username: string = '';
+  password: string = '';
+
+  ngForm: FormGroup;
 
   constructor(
+    private formBuilder: FormBuilder,
     private router: Router,
     private route: ActivatedRoute,
-  ) { }
+  ) { this.createForm(); }
 
   ngOnInit(): void {
   }
 
   iniciarSesion() {
-    console.log('Inicio sesión!');
+    
     setTimeout(() => {
       this.router.navigate(['catalogo']);
     }, 2000);
@@ -34,6 +38,13 @@ export class LoginComponent implements OnInit {
   mostrarPassword() {
     this.hide = !this.hide;
     this.showEye = !this.showEye;
+  }
+
+  createForm() {
+    this.ngForm = this.formBuilder.group({
+       user: new FormControl("", [Validators.required, Validators.minLength(5)]),
+       password: new FormControl("", [Validators.required, Validators.minLength(5)]),
+    });
   }
 
 }
